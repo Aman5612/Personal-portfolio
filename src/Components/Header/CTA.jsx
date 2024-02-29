d180493d2a7ec82b3f623cccfc47e74779ccd855
Aman Tiwari <amankumartiwari392@gmail.com>
2024-02-29 20:35:02 +0530

    Updated Resume

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a
-        href="https://drive.google.com/"
+        href="https://drive.google.com/file/d/19e7DgY89DEARq7fN9bURwRA6V_dzQafm/view?usp=sharing"
         target="_blank"
         className="btn"
         rel="noreferrer"
